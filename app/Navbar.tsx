57116f50f5103b3b69dec92b696008bf44e138c1
DESKTOP-VFB5TE0\Gyanendra Yadav <gyanendrayadav12345789@gmail.com>
2024-08-02 13:23:06 +0530

    authenicatio n

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,15 +1,31 @@
-import Link from 'next/link'
-import React from 'react'
- 
-const Navbar = () => {
-  return (
-      <div className='flex gap-8 bg-slate-200 p-5 items-center justify-center'>
-          <Link href="/" className='uppercase'>Logo</Link>
-          <Link href="/users" className='uppercase'>User</Link>
-          <Link href="/admin" className='uppercase'>Admin</Link>
-          <Link href="/" className='uppercase'>Cart</Link>
-    </div>
-  )
-}
+'use client';
+import { useSession } from "next-auth/react";
+import Link from "next/link";
+import React from "react";
 
-export default Navbar
+const Navbar = () => {
+  const { status, data: session } = useSession();
+  
+  return (
+    <div className="flex gap-8 bg-slate-200 p-5 items-center justify-center">
+      <Link href="/" className="uppercase">
+        Logo
+      </Link>
+      <Link href="/users" className="uppercase">
+        User
+      </Link>
+      <Link href="/admin" className="uppercase">
+        Admin
+      </Link>
+      {status==='loading'&& <div>Loading...</div>}
+      {status === "authenticated" && <div>{session.user!.name} <Link href='/api/auth/signout'>Sign Out</Link></div>}
+      {status === "unauthenticated" && (
+        <Link href="/api/auth/signin" className="uppercase">
+          SIGN IN
+        </Link>
+      )}
+    </div>
+  );
+};
+
+export default Navbar;
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,17 @@
 import Link from "next/link";
 import ProductCard from "./components/ProductCard";
-export default function Home() {
+import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]/route";
+export default async function Home() {
+  const session = await getServerSession(authOptions)
+
   return (
     <main>
-      <h1>Good Day!!</h1>
+      <h1>Hello {session && <span>{ session.user!.name}</span> }</h1>
       <Link href="/users">Users</Link>
       <ProductCard/>
 
     </main>
   );
+  
 }
